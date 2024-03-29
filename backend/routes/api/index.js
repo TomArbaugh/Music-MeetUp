4df38be846e283556f8c110e1f352da93c7cf268
thomas <thomasarbaugh@gmail.com>
2024-03-28 21:06:26 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 // // GET /api/require-auth
 // const { requireAuth } = require('../../utils/auth.js');
 // router.get(
