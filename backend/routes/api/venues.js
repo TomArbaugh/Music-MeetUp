eedbc83d292ffceb0a3585aaa38378f448342745
thomas <thomasarbaugh@gmail.com>
2024-04-02 20:57:35 -0700

    set up venues router

---
+++ backend/routes/api/venues.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { Op } = require('sequelize');
+// const bcrypt = require('bcryptjs');
+// const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { Venue } = require('../../db/models');
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
+
+const { requireAuth } = require('../../utils/auth.js');
+
+
+
+
+module.exports = router;
