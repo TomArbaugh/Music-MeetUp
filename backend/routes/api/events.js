d45323d128396e693189cb450acd568c98cb0598
thomas <thomasarbaugh@gmail.com>
2024-04-03 14:12:14 -0700

    connected endpoints attendances

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -2,12 +2,32 @@ const express = require('express');
 const { Op } = require('sequelize');
 // const bcrypt = require('bcryptjs');
 // const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { Event, Group, Venue, Attendance, EventImage } = require('../../db/models');
+const { Event, Group, Venue, Attendance, EventImage, User } = require('../../db/models');
 // const { check } = require('express-validator');
 // const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 const { requireAuth } = require('../../utils/auth.js');
+
+router.get('/:eventId/attendees', async (req, res) => {
+
+    const attendees = await Event.findByPk(req.params.eventId, {
+        include: [Attendance, User]
+    });
+    let Attendees = []
+    attendees.Attendances.forEach((attendee) => {
+       const user = attendees.Users.find((user) => user.id === attendee.userId);
+
+      let newAttendee = {}
+        if (attendee.id !== undefined) attendee.id = id
+        newAttendee.firstName = user.firstName
+        newAttendee.lastName = user.lastName
+        newAttendee.Attendance = {status: attendee.status}
+
+        Attendees.push(newAttendee)
+    })
+    res.json({Attendees})
+});
 
 
 router.get('/:eventId', async (req, res) => {
@@ -26,6 +46,35 @@ router.get('/', async (req, res) => {
     res.json({events})
 });
 
+router.post('/:eventId/attendance', requireAuth, async (req, res) => {
+
+    const { user } = req;
+    let safeUser;
+    if (user) {
+        safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+      }
+    }
+    const event = await Event.findByPk(req.params.eventId, {
+        include: Attendance
+    });
+
+    const newAttendance = await Attendance.create({
+        eventId: req.params.eventId,
+        userId: safeUser.id,
+        status: 'Attending'
+    })
+
+    res.json({
+        "userId": safeUser.id,
+        "status": "Attending"
+      })
+});
+
 
 router.post('/:eventId/images', requireAuth, async (req, res) => {
     const {url, preview} = req.body
@@ -37,6 +86,18 @@ router.post('/:eventId/images', requireAuth, async (req, res) => {
 
     res.json(newImage)
 });
+
+
+router.put('/:eventId/attendance', requireAuth, async (req, res) => {
+    const {userId, status} = req.body
+    const event = await Event.findByPk(req.params.eventId, {
+        include: Attendance
+    })
+    const statusChange = event.Attendances.find((attend) => parseInt(userId) === attend.userId)
+
+    statusChange.status = status
+    res.json(statusChange)
+})
 
 router.put('/:eventId', requireAuth, async (req, res) => {
     const {venueId, name, type, capacity, price, description, startDate, endDate} = req.body
@@ -57,6 +118,17 @@ router.put('/:eventId', requireAuth, async (req, res) => {
     res.json(event)
 });
 
+router.delete('/:eventId/attendance/:userId', requireAuth, async (req, res) => {
+    const userAttending = await Event.findByPk(req.params.eventId, {
+        include: Attendance
+    });
+    const attendanceToDelete = userAttending.Attendances.find((attendee) => parseInt(req.params.userId) === attendee.userId)
+
+    await attendanceToDelete.destroy()
+    res.json({
+        "message": "Successfully deleted attendance from event"
+      })
+})
 
 router.delete('/:eventId', requireAuth, async (req, res) => {
 
